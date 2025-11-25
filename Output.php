a3a74fb7274ad72821a5a6494af54f725255943b
Tanner <123541788+grimsome-tinker701@users.noreply.github.com>
2025-11-25 03:10:40 -0600

    Add files via upload
    Updated security for subfolders. added process to verify completion of all pre-processes before allowing access to output page. added rerouting to error page upon violations. 
    Started process to stop autofill --Did Not Succeed--.

--- Output.php
+++ Output.php
@@ -6,12 +6,31 @@
 <body>
     <center>
     <?php
-        session_start();
-        if(!empty($_SESSION['QuestionareApproved']) && !empty($_SESSION['File'])){$link = $_SESSION['File'];} else {$link = "No";}
-        if($link != "No"){
-            echo "<a href=\"./Filled Forms/$link\">Open File!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"./index.html\">Restart!</a>";
-        }else{die("Direct Access Is Not Allowed!!!");}
-        session_write_close();
+        try{
+            session_start();
+            if(!empty($_SESSION['QuestionareApproved']) && !empty($_SESSION['File'])){$link = $_SESSION['File'];} else {$link = "No";}
+            if($link != "No"){
+                echo "<a href=\"./Filled Forms/$link\">Open File!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"./index.php\">Restart!</a>";
+            }else{
+                header("location:./Error.html");
+                die("Direct Access Is Not Allowed!!!");
+            }
+        }catch (Exception $e){
+            $MSG = <<<EOD
+            <center>
+                <H1>!WARNING! !WARNING! !WARNING! !WARNING! !WARNING!</H1>
+                <H2 style="position:absolute; top:40%; width:100%;">!!!Error Found!!!</H2>
+                <H2 style="position:absolute; top:50%; width:100%;">$e</H2>
+                <H1 style="position:absolute; bottom:3%; width:100%;">!WARNING! !WARNING! !WARNING! !WARNING! !WARNING!</H1>
+            </center>
+            EOD;
+            die($MSG);
+        }finally{
+            $_SESSION = array();
+            session_destroy();
+        }
+        
+        
     ?>
     </center>
 </body>
